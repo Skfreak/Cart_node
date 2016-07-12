46586be3a176c38cde985a5d1490c25f4b468de5
Shashikant <chandresh41414@gmail.com>
2016-07-12 13:16:36 +0530

    First vommit

---
+++ shopping-cart/config/passport.js
@@ -0,0 +1,24 @@
+var passport = require('passport');
+var User = require('../models/user');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(user,done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id,done){
+  User.findById(id, function(err,user){
+    done(err,user);
+  });
+});
+
+passport.use('local.signup',new LocalStrategy({
+  usernameField:'email',
+  passwordField: 'password',
+  passReqToCallback: true
+},function(req,email,password, done){
+  User.findOne({'email':email},function(err,user){
+    
+  });
+
+}));
